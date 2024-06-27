d7110531ea76eb30b807c1b426feff0ce6354bfd
facudiaz16 <fdiazbordon@gmail.com>
2024-06-27 16:30:42 -0300

    agregando resto de archivos

--- app.js
+++ app.js
@@ -9,9 +9,8 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
-})
+const port = process.env.PORT || 3001;
+app.listen(port, () =>console.log(`Servidor corriendo en le puerto ${port}`));
 
 app.get('/',function(req,res){
     res.sendFile(path.resolve(__dirname, './views/home.html'));
